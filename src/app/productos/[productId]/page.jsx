01b53d2dd169fe1649b73bc7082311519ca32b83
J. Alejandro Solarte R <juan.solarte7@u.icesi.edu.co>
2025-02-11 20:01:09 -0500

    [UPDATE] Refactor metadata generation to include full product description and adjust keyword extraction

--- src/app/productos/[productId]/page.jsx
+++ src/app/productos/[productId]/page.jsx
@@ -5,9 +5,9 @@ import { getProductById } from "@/services/GetProducts";
 
 export async function generateMetadata({ params }) {
     const product = await getProductById(params.productId);
-    const { titulo, descripcion, imagenes, categorias } = product.attributes;
-    const productDescription = descripcion.map(({ type, children }) => children[0].text).slice(0, 2).join(" ");
-    const descriptionKeywords = productDescription.split(" ").slice(0, 10).join(", ");
+    const { titulo, descripcion, imagenes, categorias } = product.attributes;    
+    const productDescription = descripcion.map(({ type, children }) => children[0].text).join(" ");    
+    const descriptionKeywords = productDescription.split(" ").join(", ");
     const categoryKeywords = categorias.data.map(({ attributes }) => attributes.categoria).join(", ");
     const keywords = `${categoryKeywords}, ${descriptionKeywords}`;
 
